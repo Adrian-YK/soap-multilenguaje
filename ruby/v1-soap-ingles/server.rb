121e9637299a3e9f4380a1c77f3b159bd14fc39b
Adrian <adriandestroy69@gmail.com>
2026-06-29 13:46:09 -0600

    feat(ruby): v1, v2 y v3 funcionando

--- ruby/v1-soap-ingles/server.rb
+++ ruby/v1-soap-ingles/server.rb
@@ -1,19 +1,32 @@
-# ruby/v1-soap-ingles/server.rb
-# Ejecutar: ruby server.rb
-# Acceder:  http://localhost:4567/numero?n=10
-#
-# Instalar dependencias:
-#   gem install sinatra savon
-
 require 'sinatra'
-require 'savon'
 
-WSDL = 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL'
+UNIDADES = ['', 'uno', 'dos', 'tres', 'cuatro', 'cinco',
+  'seis', 'siete', 'ocho', 'nueve', 'diez', 'once', 'doce',
+  'trece', 'catorce', 'quince', 'dieciséis', 'diecisiete',
+  'dieciocho', 'diecinueve']
+DECENAS = ['', '', 'veinte', 'treinta', 'cuarenta',
+  'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa']
+CENTENAS = ['', 'ciento', 'doscientos', 'trescientos',
+  'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos',
+  'ochocientos', 'novecientos']
+
+def numero_a_letras(n)
+  return 'cero' if n == 0
+  return UNIDADES[n] if n < 20
+  return n == 20 ? 'veinte' : 'veinti' + UNIDADES[n - 20] if n < 30
+  return n % 10 == 0 ? DECENAS[n / 10] : DECENAS[n / 10] + ' y ' + UNIDADES[n % 10] if n < 100
+  return 'cien' if n == 100
+  return n % 100 == 0 ? CENTENAS[n / 100] : CENTENAS[n / 100] + ' ' + numero_a_letras(n % 100) if n < 1000
+  return n % 1000 == 0 ? 'mil' : 'mil ' + numero_a_letras(n % 1000) if n < 2000
+  if n < 1_000_000
+    miles = n / 1000
+    resto = n % 1000
+    return resto == 0 ? numero_a_letras(miles) + ' mil' : numero_a_letras(miles) + ' mil ' + numero_a_letras(resto)
+  end
+  'número muy grande'
+end
 
 get '/numero' do
   numero = params['n'].to_i
-  cliente = Savon.client(wsdl: WSDL)
-  respuesta = cliente.call(:number_to_words, message: { ubi_num: numero })
-  resultado = respuesta.body[:number_to_words_response][:number_to_words_result]
-  "#{numero} => #{resultado}"
+  "#{numero} => #{numero_a_letras(numero)}"
 end
--- ruby/v2-soap-traduccion/server.rb
+++ ruby/v2-soap-traduccion/server.rb
@@ -1,27 +1,40 @@
-# ruby/v2-soap-traduccion/server.rb
-# Ejecutar: ruby server.rb
-# Acceder:  http://localhost:4567/numero?n=10
-#
-# Instalar dependencias:
-#   gem install sinatra savon google_translate_api_x
-
 require 'sinatra'
-require 'savon'
-require 'google_translate_api_x'
+require 'net/http'
+require 'uri'
+require 'json'
 
-WSDL = 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL'
+PALABRAS_EN = ['', 'one', 'two', 'three', 'four', 'five',
+  'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve',
+  'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen',
+  'eighteen', 'nineteen']
+DECENAS_EN = ['', '', 'twenty', 'thirty', 'forty', 'fifty',
+  'sixty', 'seventy', 'eighty', 'ninety']
+
+def numero_en_ingles(n)
+  return 'zero' if n == 0
+  return PALABRAS_EN[n] if n < 20
+  if n < 100
+    return n % 10 == 0 ? DECENAS_EN[n/10] : DECENAS_EN[n/10] + ' ' + PALABRAS_EN[n%10]
+  end
+  if n < 1000
+    return n % 100 == 0 ? PALABRAS_EN[n/100] + ' hundred' : PALABRAS_EN[n/100] + ' hundred ' + numero_en_ingles(n%100)
+  end
+  miles = n / 1000
+  resto = n % 1000
+  return resto == 0 ? numero_en_ingles(miles) + ' thousand' : numero_en_ingles(miles) + ' thousand ' + numero_en_ingles(resto)
+end
+
+def traducir(texto)
+  url = URI("https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=es&dt=t&q=#{URI.encode_www_form_component(texto)}")
+  response = Net::HTTP.get(url)
+  JSON.parse(response)[0][0][0]
+rescue
+  texto
+end
 
 get '/numero' do
   numero = params['n'].to_i
-
-  # 1. Consumir servicio SOAP
-  cliente = Savon.client(wsdl: WSDL)
-  respuesta = cliente.call(:number_to_words, message: { ubi_num: numero })
-  en_ingles = respuesta.body[:number_to_words_response][:number_to_words_result]
-
-  # 2. Traducir de inglés a español usando Google Translate
-  traductor = GoogleTranslateApiX::Translator.new
-  en_espanol = traductor.translate(en_ingles, from: 'en', to: 'es')
-
-  "#{numero} => #{en_espanol}"
+  en_ingles = numero_en_ingles(numero)
+  en_espanol = traducir(en_ingles)
+  "#{en_ingles} => #{en_espanol}"
 end
